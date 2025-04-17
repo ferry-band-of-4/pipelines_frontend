bd03983f4262873538a12d6a3049b13f3c238f07
dheeraj.g <dheeraj.g@versa-networks.com>
2025-04-17 12:33:24 +0530

    code cleaning

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ import tempfile
 import os
 import requests
 
+
 # Replace 'http://127.0.0.1:8000/uploadfile/' with your actual endpoint URL
 def ret_url(file_name):
     print(file_name)
-    url = f'http://127.0.0.1:8000/predict_steamlit?name={file_name}'
+    url = f"http://127.0.0.1:8000/predict_steamlit?name={file_name}"
     return url
-#ENDPOINT_URL = 'http://127.0.0.1:8000/predict_steamlit?name=h01.edf'
-UPLOAD_FOLDER = 'edf_buffer'
+
+
+# ENDPOINT_URL = 'http://127.0.0.1:8000/predict_steamlit?name=h01.edf'
+UPLOAD_FOLDER = "edf_buffer"
+
 
 def load_edf_file(uploaded_file):
     # Save the .edf file content to a temporary file
     temp_file_path = tempfile.NamedTemporaryFile(delete=False).name
-    with open(temp_file_path, 'wb') as temp_file:
+    with open(temp_file_path, "wb") as temp_file:
         temp_file.write(uploaded_file.read())
 
     # Load the .edf file
@@ -36,9 +40,11 @@ def load_edf_file(uploaded_file):
 
     return signals, signal_labels
 
+
 def load_images_from_directory(directory):
-    image_files = [f for f in os.listdir(directory) if f.endswith('.png')]
+    image_files = [f for f in os.listdir(directory) if f.endswith(".png")]
     return image_files
+
 
 def call_endpoint(file_name):
     # Set the headers with the file content
@@ -57,13 +63,14 @@ def call_endpoint(file_name):
 
     return response
 
+
 def main():
     st.title("EDF File Viewer")
 
     # Upload .edf file through Streamlit
     uploaded_file = st.file_uploader("Choose an .edf file", type=["edf"])
-    
-   # print(uploaded_file.getvalue())
+
+    # print(uploaded_file.getvalue())
 
     if uploaded_file is not None:
         print(type(uploaded_file))
@@ -97,31 +104,39 @@ def main():
                 idx = row * num_columns + col
                 if idx < len(signal_labels):
                     # Display the line chart without using pyplot
-                    columns[col].line_chart(np.array(signals[idx][:1000]), use_container_width=True)
+                    columns[col].line_chart(
+                        np.array(signals[idx][:1000]), use_container_width=True
+                    )
                     columns[col].write(f"Signal {idx + 1}: {signal_labels[idx]}")
 
         # Call the endpoint when the user finishes uploading the file
         if st.button("diagnose for schzophrenia"):
-            #files = {"file": (uploaded_file.name, uploaded_file.read(), uploaded_file.type)}
+            # files = {"file": (uploaded_file.name, uploaded_file.read(), uploaded_file.type)}
             response = call_endpoint(uploaded_file.name)
 
             # Check the response status
             if response.status_code == 200:
                 st.success("FastAPI Endpoint called successfully!")
             else:
-                st.error(f"Failed to call the FastAPI Endpoint. Status code: {response.status_code}")
+                st.error(
+                    f"Failed to call the FastAPI Endpoint. Status code: {response.status_code}"
+                )
 
             st.subheader("Response from FastAPI Endpoint")
             url_predict = "http://127.0.0.1:8000/predict"
             response = requests.get(url_predict)
             st.markdown(response.json())
-            ans = response.json()['prediction']
+            ans = response.json()["prediction"]
 
             image_directory = "grad_cams"
             image_files = load_images_from_directory(image_directory)
 
-            skizo_positive_text = "## You have a high possibility of having schizophrenia"
-            skizo_negative_text = "## You have a low possibility of having schizophrenia"
+            skizo_positive_text = (
+                "## You have a high possibility of having schizophrenia"
+            )
+            skizo_negative_text = (
+                "## You have a low possibility of having schizophrenia"
+            )
 
             if not image_files:
                 st.warning("No images found in the directory!")
@@ -129,19 +144,17 @@ def main():
                 for image_file in image_files:
                     image_path = os.path.join(image_directory, image_file)
                     st.image(image_path, caption=image_file, use_column_width=True)
-            if ans == 'Schizo positive':
-                #st.markdown("you have :" + ans)
+            if ans == "Schizo positive":
+                # st.markdown("you have :" + ans)
                 st.write(skizo_positive_text)
             else:
-                #st.markdown("you have :" + ans)
+                # st.markdown("you have :" + ans)
                 st.write(skizo_negative_text)
-            #st.markdown("you have :" + ans)
+            # st.markdown("you have :" + ans)
             requests.get("http://127.0.0.1:8000/clear_dir")
             prediction_image_path = "predictions.png"
-            st.image(prediction_image_path, caption='bar graph', use_column_width=True)
+            st.image(prediction_image_path, caption="bar graph", use_column_width=True)
             os.remove(prediction_image_path)
-
-
 
 
 if __name__ == "__main__":
--- back_app.py
+++ back_app.py
@@ -31,36 +31,38 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         img_name = os.path.join(self.folder_path, self.images[idx])
-        channel_name = self.images[idx].split(".")[0]   
+        channel_name = self.images[idx].split(".")[0]
 
         image = Image.open(img_name).convert("RGB")
-        
 
         if self.transform:
             image = self.transform(image)
 
         return image, channel_name
-    
+
+
 class resnet(nn.Module):
     def __init__(self, num_classes=1):
         super().__init__()
 
-        resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+        resnet = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", pretrained=True)
 
-        self.convolutional_layers = nn.Sequential(resnet.conv1,
-                                   resnet.bn1,
-                                   resnet.relu,
-                                   resnet.maxpool,
-                                   resnet.layer1,
-                                   resnet.layer2,
-                                   resnet.layer3,
-                                   resnet.layer4)
+        self.convolutional_layers = nn.Sequential(
+            resnet.conv1,
+            resnet.bn1,
+            resnet.relu,
+            resnet.maxpool,
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4,
+        )
 
         # # Add back the batch norm layer that we removed
-        self.avgpool = resnet.avgpool # Global Average pooling layer
+        self.avgpool = resnet.avgpool  # Global Average pooling layer
 
         # # # Custom fully connect layer
-        self.fc = nn.Linear(in_features=resnet.fc.in_features, out_features=num_classes) 
+        self.fc = nn.Linear(in_features=resnet.fc.in_features, out_features=num_classes)
 
         # gradient placeholder
         self.gradient = None
@@ -68,173 +70,180 @@ class resnet(nn.Module):
     # hook for the gradients
     def activations_hook(self, grad):
         self.gradient = grad
-    
+
     def get_gradient(self):
         return self.gradient
-    
+
     def get_activations(self, x):
         return self.convolutional_layers(x)
-    
-    def forward(self, images):        
+
+    def forward(self, images):
         # Convolutional layers of resnet18
-        x = self.convolutional_layers(images) 
-               
+        x = self.convolutional_layers(images)
         # # The layers after the Conv you used the hook on
         h = x.register_hook(self.activations_hook)
-        
+
         # Global average pooling layer from resnet18
         x = self.avgpool(x)
-        
+
         x = x.reshape(x.size(0), -1)
         logits = self.fc(x)
-        
+
         output = F.sigmoid(logits)
         return output
-    
+
 
 @profile
 def plot_heatmap(denorm_image, pred, heatmap, channel_name):
 
-    plt.close('all')
+    plt.close("all")
 
-    fig, (ax1, ax2, ax3) = plt.subplots(figsize=(20,20), ncols=3)
+    fig, (ax1, ax2, ax3) = plt.subplots(figsize=(20, 20), ncols=3)
 
-    classes = ['schizophrenic', 'healthy']
+    classes = ["schizophrenic", "healthy"]
     ps = F.sigmoid(pred).cpu().detach().numpy()
     ax1.imshow(denorm_image)
-    ax1.axis('off')
+    ax1.axis("off")
 
-    ax2.barh(classes, [pred[0].item(), 1-pred[0].item()])
+    ax2.barh(classes, [pred[0].item(), 1 - pred[0].item()])
     ax2.set_aspect(0.1)
     ax2.set_yticks(classes)
     ax2.set_yticklabels(classes)
-    ax2.set_title('Predicted Class')
+    ax2.set_title("Predicted Class")
     ax2.set_xlim(0, 1.1)
 
     ax3.imshow(denorm_image)
-    ax3.imshow(heatmap, cmap='jet', alpha=0.7)
-    ax3.axis('off')
+    ax3.imshow(heatmap, cmap="jet", alpha=0.7)
+    ax3.axis("off")
 
-    plt.savefig(f"grad_cams/{channel_name}.png", bbox_inches='tight', pad_inches=1, format='png')
-    
+    plt.savefig(
+        f"grad_cams/{channel_name}.png", bbox_inches="tight", pad_inches=1, format="png"
+    )
+
 
 @profile
 def get_gradcam(model, image, prediction, size):
     print("prediction...........")
     print(prediction)
-    prediction.backward(retain_graph=True)    
+    prediction.backward(retain_graph=True)
     gradients = model.get_gradient()
-    pooled_gradients = torch.mean(gradients, dim = [0, 2, 3])
-    activations = model.get_activations(image).detach() # A1, A2, ..., Ak
-    
+    pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
+    activations = model.get_activations(image).detach()  # A1, A2, ..., Ak
+
     for i in range(activations.shape[1]):
         activations[:, i, :, :] *= pooled_gradients[i]
 
-    heatmap = torch.mean(activations, dim = 1).squeeze().cpu()
+    heatmap = torch.mean(activations, dim=1).squeeze().cpu()
     heatmap = nn.ReLU()(heatmap)
     heatmap /= torch.max(heatmap)
     heatmap = cv2.resize(heatmap.numpy(), (size, size))
-    
+
     return heatmap
 
-def mutli_grads(model, images, predictions, channel_names,size):
-   # heatmaps = []
-   # print("predictionssssssssss...........")
+
+def mutli_grads(model, images, predictions, channel_names, size):
     for i in range(len(images)):
-       # print(predictions[i])
         heatmap = get_gradcam(model, images[i].unsqueeze(0), predictions[i], size)
-        denorm_image= images[i].permute(1, 2, 0) * np.array((0.5, 0.5, 0.5)) + np.array((0.5, 0.5, 0.5))
-        plot_heatmap(denorm_image, predictions[i], heatmap,channel_names[i])
-        #plt.imsave(f"grad_cams/{channel_names[i]}.png", heatmap, cmap='jet')
-        
-
-
+        denorm_image = images[i].permute(1, 2, 0) * np.array(
+            (0.5, 0.5, 0.5)
+        ) + np.array((0.5, 0.5, 0.5))
+        plot_heatmap(denorm_image, predictions[i], heatmap, channel_names[i])
 
 
 app = FastAPI()
-preprocess = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+preprocess = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
 
-device= torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = resnet(num_classes=1).to(device)
 model_num = 20
 checkpoint_path = ""
-checkpoint = torch.load(f'resnet-finetune-adam-epoch_{model_num}.pth', map_location=device)
-model.load_state_dict(checkpoint['model_state_dict'])
+
+checkpoint = torch.load("resnet-finetune-adam-epoch_20", map_location=device)
+model.load_state_dict(checkpoint["model_state_dict"])
+
 
 @profile
 def plot_figs(path, target_path):
     sfreq = 250
     raw = mne.io.read_raw_edf(path, preload=True)
-    train_idx = np.arange(0, 30*sfreq, dtype=int)
+    train_idx = np.arange(0, 30 * sfreq, dtype=int)
     gmw = MorseWavelet(gamma=2, beta=1)
-    asr_instance = asr.ASR(method='euclid')
+    asr_instance = asr.ASR(method="euclid")
     channels = raw.ch_names
     data = raw._data
-    asr_data, _ =  asr_instance.fit(data[:, train_idx])
+    asr_data, _ = asr_instance.fit(data[:, train_idx])
 
     dict_eeg = {channels[i]: asr_data[i] for i in range(19)}
-    
 
-    i=0
-    for channel_name , channel_data in dict_eeg.items():
+    i = 0
+    for channel_name, channel_data in dict_eeg.items():
         Wxh, *_ = ghostipy.spectral.cwt(channel_data, wavelet=gmw, voices_per_octave=10)
-        #plt.figure(figsize=(8,6))
-        plt.imshow(np.abs(Wxh), aspect='auto', cmap='turbo')
-        plt.axis('off')
-        image_file_name = channel_name+".png"
+        # plt.figure(figsize=(8,6))
+        plt.imshow(np.abs(Wxh), aspect="auto", cmap="turbo")
+        plt.axis("off")
+        image_file_name = channel_name + ".png"
         image_path = os.path.join(target_path, image_file_name)
-        plt.savefig(image_path, bbox_inches='tight', pad_inches=0, format='png')
+        plt.savefig(image_path, bbox_inches="tight", pad_inches=0, format="png")
         print(plt.gcf().get_size_inches())
         plt.close()
-        i+=1
-        subprocess.run(['sync'])
+        i += 1
+        subprocess.run(["sync"])
         gc.collect()
     gc.collect()
-    plt.close('all')
+    plt.close("all")
 
-    print(dict_eeg, asr_data)    
+    print(dict_eeg, asr_data)
     return True
+
 
 @profile
 def image_transform(image_folder):
 
-    preprocess = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  
-    ])
+    preprocess = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
 
-    image_files = [f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))]
+    image_files = [
+        f
+        for f in os.listdir(image_folder)
+        if os.path.isfile(os.path.join(image_folder, f))
+    ]
 
     for image_file in image_files:
-    
+
         input_path = os.path.join(image_folder, image_file)
         image = Image.open(input_path)
 
-        if image.mode == 'RGBA':
-            image = image.convert('RGB')
-        
+        if image.mode == "RGBA":
+            image = image.convert("RGB")
 
         transformed_image = preprocess(image)
 
         pil_tranformed_image = transforms.ToPILImage()(transformed_image)
 
         pil_tranformed_image.save(input_path)
-    
+
     print("transformation to RGB completed")
 
     return True
 
+
 @profile
-def dir_cleaner(edf_buffer:str, image_buffer:str):
-    image_patter_dir = os.path.join(image_buffer, '*.png')
+def dir_cleaner(edf_buffer: str, image_buffer: str):
+    image_patter_dir = os.path.join(image_buffer, "*.png")
     edf_pattern_dir = os.path.join(edf_buffer, "*.edf")
     grad_cam_pattern_dir = os.path.join("grad_cams", "*.png")
-    
+
     img_files_glob = glob.glob(image_patter_dir)
     efd_files_glob = glob.glob(edf_pattern_dir)
     grad_cam_files_glob = glob.glob(grad_cam_pattern_dir)
@@ -242,7 +251,7 @@ def dir_cleaner(edf_buffer:str, image_buffer:str):
     for file_path in img_files_glob:
         try:
             os.remove(file_path)
-            #print(f"deleted {file_path}")
+            # print(f"deleted {file_path}")
         except Exception as e:
             print(e)
             print(f" error deleting image {file_path}")
@@ -267,143 +276,151 @@ def dir_cleaner(edf_buffer:str, image_buffer:str):
 
 @profile
 def new_preprocess(image_folder):
-    transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  
-
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
 
     custom_dataset = CustomDataset(image_folder, transform)
     batch_size = 19
-    data_loader = torch.utils.data.DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
+    data_loader = torch.utils.data.DataLoader(
+        custom_dataset, batch_size=batch_size, shuffle=True
+    )
     return data_loader
 
 
 @profile
 def plot_predictions(prediction_dict):
-    plt.close('all')
-    plt.rcParams['figure.facecolor'] = 'black'
-    plt.rcParams['axes.facecolor'] = 'black'
+    plt.close("all")
+    plt.rcParams["figure.facecolor"] = "black"
+    plt.rcParams["axes.facecolor"] = "black"
 
     plt.figure(figsize=(20, 10))
-    colors = ['tomato' if value > 0.5 else 'skyblue' for value in prediction_dict.values()]
-    values = [values if values > 0.5 else 1-values for values in prediction_dict.values()]
+    colors = [
+        "tomato" if value > 0.5 else "skyblue" for value in prediction_dict.values()
+    ]
+    values = [
+        values if values > 0.5 else 1 - values for values in prediction_dict.values()
+    ]
     bars = plt.bar(prediction_dict.keys(), values, color=colors)
-    plt.yticks(color='white')
-    plt.xticks(rotation=90, color='white')  
-    plt.bar_label(plt.bar(prediction_dict.keys(), values, color=colors), labels=[round(value, 2) for value in values], padding=3, color='white')
+    plt.yticks(color="white")
+    plt.xticks(rotation=90, color="white")
+    plt.bar_label(
+        plt.bar(prediction_dict.keys(), values, color=colors),
+        labels=[round(value, 2) for value in values],
+        padding=3,
+        color="white",
+    )
     plt.ylim(0, 1.5)
-
 
     # Add legend with custom labels
     if "tomato" not in colors:
-        legend_labels = ['Healthy']
+        legend_labels = ["Healthy"]
     elif "skyblue" not in colors:
-        legend_labels = ['Schizophrenic']
+        legend_labels = ["Schizophrenic"]
     else:
-        legend_labels = ['Healthy', 'Schizophrenic']
+        legend_labels = ["Healthy", "Schizophrenic"]
 
-    
-    legend = plt.legend(bars, legend_labels, loc='upper right', fontsize='large', frameon=False, title='Class',  labelcolor='white')
+    legend = plt.legend(
+        bars,
+        legend_labels,
+        loc="upper right",
+        fontsize="large",
+        frameon=False,
+        title="Class",
+        labelcolor="white",
+    )
 
     # Set legend title color
-    legend.get_title().set_color('white')
+    legend.get_title().set_color("white")
 
     # Set color of X and Y axes to white
     ax = plt.gca()
-    ax.spines['bottom'].set_color('white')
-    ax.spines['left'].set_color('white')
-    plt.savefig("predictions.png", bbox_inches='tight', pad_inches=0, format='png', facecolor='black')
+    ax.spines["bottom"].set_color("white")
+    ax.spines["left"].set_color("white")
+    plt.savefig(
+        "predictions.png",
+        bbox_inches="tight",
+        pad_inches=0,
+        format="png",
+        facecolor="black",
+    )
     plt.close()
     return True
 
 
 @profile
 def model_prediction(data_loader, model):
-    model.eval()   
+    model.eval()
     prediction_dict = {}
-    actual_preds ={}
+    actual_preds = {}
     for images, channel_names in data_loader:
         images = images.to(device)
         outputs = model(images)
-        # print("outttttttttpuuuuuuuuuttttttt")
-        # print(outputs)
-        # print(outputs[0])
-        # print("ourputttttt overrrrrrrrrr")
-        mutli_grads(model, images, outputs,channel_names, 224)
+        mutli_grads(model, images, outputs, channel_names, 224)
         round_outputs = torch.round(outputs)
-        
+
         for i in range(len(round_outputs)):
             prediction_dict[channel_names[i]] = round_outputs[i].item()
             actual_preds[channel_names[i]] = outputs[i].item()
 
     plot_predictions(actual_preds)
-    
+
     return prediction_dict
 
-
-    
 
 UPLOAD_FOLDER = "edf_buffer"
 IMAGE_BUFFER = "image_buffer"
 
+
 def main():
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    subprocess.run(['sync'])
+    subprocess.run(["sync"])
     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
     file_name = file.filename
-    # Create the upload folder if it doesn't exis
-    plt.close('all')
+    plt.close("all")
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
     with open(file_path, "wb") as buffer:
         buffer.write(file.file.read())
 
-    
+    plot_figs(file_path, IMAGE_BUFFER)
+    return {"filename": file_name, "saved_path": file_path, "removed": True}
 
-    #raw = mne.io.read_raw_edf('edf_buffer/h01.edf', preload=True)
-    plot_figs(file_path,IMAGE_BUFFER)
-    #image_transform(IMAGE_BUFFER)
-
-    #print(raw.__dict__)
-    
-
-    # os.remove(file_path)
-    # folder_path = "image_buffer"
-    # png_files = glob.glob(os.path.join(folder_path, "*.png"))
-    # for file in png_files:
-    #     os.remove(file)
-    return {"filename": file_name, "saved_path": file_path, "removed":True}
 
 @app.get("/clear_dir")
 def clear_dir():
-    dir_cleaner(UPLOAD_FOLDER,IMAGE_BUFFER)
+    dir_cleaner(UPLOAD_FOLDER, IMAGE_BUFFER)
 
     return True
+
 
 @app.get("/predict")
 def predict():
     data_loader = new_preprocess(IMAGE_BUFFER)
     prediction_dict = model_prediction(data_loader, model)
     values = list(prediction_dict.values())
-    count =0
+    count = 0
     for value in values:
         if value == 1:
-            count+=1
+            count += 1
 
     if count >= 19 - count:
         prediction_dict["prediction"] = "Schizo positive"
     else:
         prediction_dict["prediction"] = "Schizo negative"
-    
-    
 
     return prediction_dict
+
 
 @app.get("/predict_steamlit")
 async def predict_steamlit(name: str):
@@ -411,10 +428,8 @@ async def predict_steamlit(name: str):
     file_path = os.path.join(UPLOAD_FOLDER, name)
     print(file_path)
 
-
-    plot_figs(file_path,IMAGE_BUFFER)
+    plot_figs(file_path, IMAGE_BUFFER)
     return {"name": name}
-    
 
 
 if __name__ == "__main__":
